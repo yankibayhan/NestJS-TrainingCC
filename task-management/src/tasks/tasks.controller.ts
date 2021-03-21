0653ab05042c556ae4c792f541f5d7d857a5a15e
yankibayhan <yanki.bayhan@clever.codes>
2021-03-21 17:30:11 +0100

    GET Request for tasks added

--- task-management/src/tasks/tasks.controller.ts
+++ task-management/src/tasks/tasks.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private tasksService : TasksService){ } // Provided injectable service 
+
+
+    @Get() // provided a decorator for GET method
+    getTasks(){
+        return this.tasksService.getAllTasks();
+    }
+}
+
