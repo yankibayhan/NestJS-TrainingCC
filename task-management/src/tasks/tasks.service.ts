edd09c4a1431b399a7e84808f40a5d5a8b082341
yankibayhan <yanki.bayhan@clever.codes>
2021-03-23 14:24:08 +0100

    Error handling and validation pipes added

--- task-management/src/tasks/tasks.service.ts
+++ task-management/src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task, TaskStatus } from './task.model';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 
 @Injectable()
@@ -12,9 +13,29 @@ export class TasksService {
         return this.tasks;
     }
 
+    getTasksWithFilters(filterDto : GetTasksFilterDto): Task[] {
+        const {status, search} = filterDto;
+        let tasks = this.getAllTasks();
+        if (status) {
+            tasks = tasks.filter(task => task.status === status)
+        }
+        if (search) {
+            tasks = tasks.filter(task =>
+                task.title.includes(search) ||
+                task.description.includes(search),
+            );
+        }
+        return tasks;
+    }
+
     // Get Task from its ID
     getTaskById(id : string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+
+        if (!found) {
+            throw new NotFoundException(`Task with ${id} not found`);
+        }
+        return found;
     }
     
     // Create a Task
@@ -33,7 +54,8 @@ export class TasksService {
     }
     // Delete a Task
     deleteTaskById(id : string): void {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id !== found.id);
     }
 
     // Update Task Status
