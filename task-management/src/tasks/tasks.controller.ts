a027076dffa8c36ab6d6ddfe3957db052c17403b
yankibayhan <yanki.bayhan@clever.codes>
2021-03-23 11:26:58 +0100

    Deleting Tasks Functionality added

--- task-management/src/tasks/tasks.controller.ts
+++ task-management/src/tasks/tasks.controller.ts
@@ -1,8 +1,10 @@
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TasksService } from './tasks.service';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Task } from './task.model';
 
+
+// Big Magic Happens in Controller
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService : TasksService){} // Provided injectable service 
@@ -17,11 +19,15 @@ export class TasksController {
     getTaskById(@Param('id') id : string): Task {
         return this.tasksService.getTaskById(id);
     }
-    
 
     @Post() // provided a decorator for POST method
     createTask(@Body() createTaskDto : CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Delete(':id') // provided id url path to delete
+    deleteTaskById(@Param('id') id: string): void {
+        this.tasksService.deleteTaskById(id);
     }
 }      
 
--- task-management/src/tasks/tasks.service.ts
+++ task-management/src/tasks/tasks.service.ts
@@ -7,12 +7,17 @@ import { v4 as uuid } from 'uuid';
 export class TasksService {
     private tasks: Task[] = [];
 
+    // Fork all the tasks
     getAllTasks(): Task[] {
         return this.tasks;
     }
+
+    // Get Task from its ID
     getTaskById(id : string): Task {
         return this.tasks.find(task => task.id === id);
     }
+    
+    // Create a Task
     createTask(createTaskDto : CreateTaskDto) {
 
         const {title, description} = createTaskDto;
@@ -25,6 +30,10 @@ export class TasksService {
         };
         this.tasks.push(task);
         return task;
+    }
+    // Delete a Task
+    deleteTaskById(id : string): void {
+        this.tasks = this.tasks.filter(task => task.id !== id);
     }
     
 }
