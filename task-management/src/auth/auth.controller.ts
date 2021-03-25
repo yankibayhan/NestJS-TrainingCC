a5e0f3663bdb45d06b5589487f989579d1147835
yankibayhan <yanki.bayhan@clever.codes>
2021-03-25 14:11:48 +0100

    User Auth Validation

--- task-management/src/auth/auth.controller.ts
+++ task-management/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { Controller, Post, Get, Patch, Delete, Body } from '@nestjs/common';
+import { Controller, Post, Get, Patch, Delete, Body, ValidationPipe } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
@@ -8,7 +8,7 @@ export class AuthController {
     constructor(private authService: AuthService) {}
     
     @Post('signup')
-    signUp(@Body() credentialDto: AuthCredentialsDto): Promise<void> {
+    signUp(@Body(ValidationPipe) credentialDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signup(credentialDto);
     }
 }
