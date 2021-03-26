1326db0e2df6cd472ba6a51b979df4933adb9c03
yankibayhan <yanki.bayhan@clever.codes>
2021-03-26 13:38:42 +0100

    Passport Strategy added

--- task-management/src/auth/auth.module.ts
+++ task-management/src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { JwtStrategy } from './jwt-strategy';
 import { UserRepository } from './user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
@@ -18,6 +19,13 @@ import { PassportModule } from '@nestjs/passport';
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    JwtStrategy,
+  ],
+  exports: [
+    JwtStrategy,
+    PassportModule
+  ],
 })
 export class AuthModule {}
---
+++ task-management/src/auth/jwt-strategy.ts
@@ -0,0 +1,35 @@
+import { UserRepository } from './user.repository';
+import { JwtPayload } from './jwt-payload-interface';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+
+// JWT Strategy
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository : UserRepository,
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: 'topSecret51',
+        });
+    }
+
+    async validate(payload: JwtPayload): Promise<User> {
+        
+        const {username} = payload;
+        const user = await this.userRepository.findOne({username});
+
+        if (!user) {
+            throw new UnauthorizedException()
+        }
+        
+        return user;
+    }
+
+}
