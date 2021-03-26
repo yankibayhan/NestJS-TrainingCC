76286d0adebb281b89811159cf2295eb2b388ba1
yankibayhan <yanki.bayhan@clever.codes>
2021-03-26 12:42:21 +0100

    JWT defaultStrategy implemented

--- task-management/src/auth/auth.module.ts
+++ task-management/src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports : [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: 'topSecret51',
       signOptions:  {
