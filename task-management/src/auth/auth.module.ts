20be56e959350e94f6bfb8bd405ec6959062a3e9
yankibayhan <yanki.bayhan@clever.codes>
2021-03-26 12:39:18 +0100

    JWT secret registered

--- task-management/src/auth/auth.module.ts
+++ task-management/src/auth/auth.module.ts
@@ -1,12 +1,18 @@
 import { UserRepository } from './user.repository';
-import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports : [
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions:  {
+        expiresIn: 3600,
+      }
+    }),
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
