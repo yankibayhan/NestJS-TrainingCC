5868e69d013e36863c5f0b06ab0b6109c6f7f6ca
yankibayhan <yanki.bayhan@clever.codes>
2021-03-27 18:17:55 +0100

    port configuration completed

--- task-management/src/main.ts
+++ task-management/src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config'; // Imported Configuration Package
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap'); // Added Logger
 
   const app = await NestFactory.create(AppModule);
 
-  const port = 3000; // Defined port num
+  const port = process.env.PORT || serverConfig.port; // Defined port num
   await app.listen(port);
-  logger.log(`Application listening on port ${port}`)
+  logger.log(`Application listening on port ${port}`);
   
 }
 bootstrap();
