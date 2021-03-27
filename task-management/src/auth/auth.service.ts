e9dbce0d24938ef1ed07f353ecfe2944b32a5ba4
yankibayhan <yanki.bayhan@clever.codes>
2021-03-27 16:58:16 +0100

    Added debug logger for AuthService

--- task-management/src/auth/auth.service.ts
+++ task-management/src/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { JwtPayload } from './jwt-payload-interface';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+
+    private logger = new Logger('AuthService') // Added logger for AuthService
     constructor(
         @InjectRepository(UserRepository)
         private userRepository : UserRepository, // Injected User Repository
@@ -26,6 +28,7 @@ export class AuthService {
 
         const payload: JwtPayload = { username };
         const accessToken = await this.jwtService.sign(payload);
+        this.logger.debug(`Generated JWT token with payload "${JSON.stringify(payload)}" `); //Debug logger
 
         return { accessToken };
     }
